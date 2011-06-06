7b6de8bc540d921a08557c8711ff005f158b6bdf
mindfreakchocz <mindfreakchocz@gmail.com>
2011-06-06 05:51:42 -0700

    Edited examples/example.php via GitHub

--- examples/example.php
+++ examples/example.php
@@ -1,26 +1,18 @@
 <?php
 /**
- * Copyright 2011 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+ <?php
+// Awesome Facebook Application
+//
+// Name: Behind the coin
+//
 
-require '../src/facebook.php';
+require_once 'facebook-php-sdk/src/facebook.php';
 
-// Create our Application instance (replace this with your appId and secret).
+// Create our Application instance.
 $facebook = new Facebook(array(
-  'appId'  => '117743971608120',
-  'secret' => '943716006e74d9b9283d4d5d8ab93204',
+  'appId' => '116500995100273',
+  'secret' => 'f33cf502ebd22430a3a4afcc341fe2cc',
+  'cookie' => true,
 ));
 
 // Get User ID
